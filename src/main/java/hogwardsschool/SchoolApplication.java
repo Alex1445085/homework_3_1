9bbb8bf3a3ca8956bb1e01a10cb7024404a87875
Alex1445085 <n1445085@yandex.ru>
2024-11-07 09:39:54 +0300

    initial commit

---
+++ src/main/java/hogwardsschool/SchoolApplication.java
@@ -0,0 +1,11 @@
+package hogwardsschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolApplication.class, args);
+	}
+}
